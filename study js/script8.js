427a6f409d9bd2c976a46965de48b74e9e15dcd9
Sluviiik <slavalinevich13@gmail.com>
2023-06-30 09:02:39 +0200

    just more lesseon of js

---
+++ study js/script8.js
@@ -0,0 +1,19 @@
+// Massive 
+// ['orange', 'tomato', 'bananaana']
+const fruits = ['orange', 'banana', 'cherry']
+console.log(fruits)
+
+const numbers = [19, 17, 18, 20, 23]
+console.log(numbers)
+
+let usersArray = [
+	{name: 'Slava', age: 19 ,hobby: 'Dungeos & Dragons'},
+	{name: 'Nikita', age: 19 ,hobby: 'Flower'}
+]
+console.log(usersArray)
+
+const newFollower = true
+if (newFollower) {
+	usersArray.push({name: 'Nikita', age: 19 ,hobby: 'Flower'},{name: 'Giga-chad', age: 19 ,hobby: 'Flower'},{name: 'Natalia', age: 19 ,hobby: 'Flower'})
+	console.log(usersArray)
+}
